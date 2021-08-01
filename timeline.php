0dfe439c0a65632f3e636fcc79ff71d5f00cd87a
Ephraim Becker <emb16@outlook.com>
2021-08-01 12:01:56 -0400

    Added more content.

--- timeline.php
+++ timeline.php
@@ -48,9 +48,39 @@
         <iframe width="560" height="315" src="https://www.youtube.com/embed/mdshgnTA4Ac" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
       <div style="background-color: lightgrey;">
-        <h2>Summer 2007</h2>
+        <h2>August 2007</h2>
         <p>Camp Morris</p>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Yp6NX-Db9BI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div style="background-color: lightgrey;">
+        <h2>July 2012</h2>
+        <p>Camp Kaylie</p>
+        <p>Finally went to my first sleep-away camp. This camp is for people with all abilities mixing in mainstream and special needs kids. Unfortunately, I got obsessed with the "Waving Flags" song and people in my bunk got annoyed at me.</p>
+      </div>
+      <div style="background-color: lightgrey;">
+        <h2>July 2013</h2>
+        <p>Camp Kaylie</p>
+        <p>Went to Camp Kaylie again. I was very hyperactive there like I always was. I was doing pranks on people that summer and people got annoyed at me again. I just didn't understand the concept of friends. I was just bored and if only I knew that I can get positive attention, I would have the same fun but happier and other people would be happy with me as well.</p>
+      </div>
+      <div style="background-color: lightgrey;">
+        <h2>2013-2014</h2>
+        <p>YALA school in torah v'dath</p>
+        <p>Switched schools to the special needs YALA from the mainstream Darchei. Finally found out that I have autism after my parents have been hiding it from me all these years. It finally explained why I was different from everyone else. It was revealed to me from a paper of other people's diagnosis hanging around. I didn't understand anything about what a real friend was. I had people pretending to be my friend in the mainstream school, Yeshiva Darchei Torah, that I switched from. I thought that friends were just boring people. There was someone else on the autism spectrum that wanted to be my friend (for real). The problem is that he didn't give me space (I have that problem also) and got uncomfortable with him. I was wondering why I was always being lumped together with this person. I didn't know what autism was until a year later. I went to far with what I did by bullying him. I never knew that there was such a thing as people needing space. I feel ashamed of myself for what I did to him. That school, YALA, closed down because of mis-communication from staff. If only the staff could explain to me what was going on and what autism actually is but they didn't. They just treated me and the other person I was bullying as troublemakers.</p>
+      </div>
+      <div style="background-color: lightgrey;">
+        <h2>July 2014</h2>
+        <p>Camp Kaylie</p>
+        <p>Was mad at that person for not giving me space. I just didn't know how to express myself. Kept bothering that person and no one knew how to get through to me. In addition, I just got on medication for the first time and got curious about other people's medications and began looking at their medications which got people mad. If only I would get the concept of what friends are. I came home from camp not knowing why things are the way they were and why that person I bullied was fully on my mind and had more privileges than me. I was all rigid and mad. If only I knew why...</p>
+      </div>
+      <div style="background-color: lightgrey;">
+        <h2>August 2014</h2>
+        <p>Camp Chaverim</p>
+        <p>My parents realized that I was really special needs and looked at another camp for me and finally found Camp Chaverim. I finally made two friends there (for real this time (Yes, it took me until the last day of camp to appreciate those two friends.)). Finally, I understood myself much better. You need to be nice to people and get positive attention in order to be happy.</p>
+      </div>
+      <div style="background-color: lightgrey;">
+        <h2>2014-2015</h2>
+        <p>IVDU Upper School</p>
+        <p>Switched to another special needs school hoping to fix my mistakes and make friends. I had a successful first day but things turned bad after the weekend. I became hyperactive and stamped things all over my property and face. The person next to me got mad and turned the whole school against me. his got me into defense mode and made it look like I was mean to other people. I started obsessing over my best friend I made in Camp Chaverim, which in return, annoyed other people.</p>
       </div>
     </main>
   </body>
