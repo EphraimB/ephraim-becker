0f517273ef9febb3a4dc0f7b6db7a45ea019b957
Ephraim Becker <emb16@outlook.com>
2021-08-01 13:01:45 -0400

    Finished the about page.

--- about.php
+++ about.php
@@ -18,7 +18,7 @@
       <h1 style="font-weight: bold;">About</h1>
     </header>
     <main>
-
+      <p>This website was made so people can learn a lesson from my life. Parents should definitely tell their child(ren) about their diagnosis. It's very important so they can know why things are the way they are. <a target="_blank" href="https://www.aspergerexperts.com/blogs/entry/25-defense-mode-why-people-with-aspergers-seem-stuck-shutdown-so-often/">Defense mode</a> is a very important thing to understand both for the parent and the child. Also, don't look at the DSM. Instead, look at <a target="_blank" href="https://www.aspergerexperts.com/">www.aspergerexperts.com</a>. They have everything there including resourses, explanations, forums, coaching, therapy, etc.</p>
     </main>
   </body>
 </html>
