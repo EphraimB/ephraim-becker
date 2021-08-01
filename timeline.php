b16c393d3122121a120ff3f7b283a652c8e56d27
Ephraim Becker <emb16@outlook.com>
2021-08-01 10:58:44 -0400

    Started work on the timeline.

---
+++ timeline.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ephraim Becker - timeline</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li><a id="first" href="index.php"><img src="img/ephraim-becker.jpg" width="50px" height="auto"></a></li>
+        <li><a href="#">> Timeline <</a></li>
+        <li><a href="contact.php">Contact me</a></li>
+        <li><a href="about.php">About</a></li>
+      </ul>
+    </nav>
+    <main>
+      <header>
+        <h1 style="font-weight: bold;">Timeline</h1>
+      </header>
+      <div style="background-color: lightgrey;">
+        <h2>July 19, 1996</h2>
+        <p>I was born</p>
+      </div>
+      <div style="background-color: lightgrey;">
+        <h2>July 26, 1996</h2>
+        <p>My ברית</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/_H-NsuCaajU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div style="background-color: lightgrey;">
+        <h2>August 18, 1996</h2>
+        <p>My פדיון הבן</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/boNnsiCP5AA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div style="background-color: lightgrey;">
+        <h2>June 24, 1999</h2>
+        <p>My upsherin</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/W-zU1z_DNmw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </main>
+  </body>
+</html>
