228b0be909c608d69bfcf3c446514e464c484802
Ephraim Becker <emb16@outlook.com>
2021-08-01 15:34:25 -0400

    Finished

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
     <meta charset="utf-8">
     <title>Ephraim Becker</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/ephraim_becker.ico" type="image/x-icon" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
