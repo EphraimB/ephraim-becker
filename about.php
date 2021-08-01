3c8843003ce3a3541ce7ce0835aeaf62f1c7005b
Ephraim Becker <emb16@outlook.com>
2021-08-01 12:53:14 -0400

    Barebones for about page.

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ephraim Becker - timeline</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li><a id="first" href="index.php"><img src="img/ephraim-becker.jpg" width="50px" height="auto"></a></li>
+        <li><a href="#">Timeline</a></li>
+        <li><a href="contact.php">Contact me</a></li>
+        <li><a href="about.php">> About <</a></li>
+      </ul>
+    </nav>
+    <header>
+      <h1 style="font-weight: bold;">About</h1>
+    </header>
+    <main>
+      
+    </main>
+  </body>
+</html>
