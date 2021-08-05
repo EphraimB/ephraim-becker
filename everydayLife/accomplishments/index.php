878a78b2d98c10eded1d4814a0d6cba80f660430
Ephraim Becker <emb16@outlook.com>
2021-08-05 10:58:45 -0400

    Added my Amtrak trip to the site.

--- everydayLife/accomplishments/index.php
+++ everydayLife/accomplishments/index.php
@@ -49,6 +49,11 @@
               <p>Click to view</p>
             </a>
           </div>
+          <div style="background-color: green;" class="card">
+            <h2>I took Amtrak from New York to Florida by myself</h2>
+            <p>I took Amtrak from NY Penn Station to Deerfield Beach, FL for Passover in 2018 (March 28 to be exact). I got Red Cap disability services at NY Penn Station because of my autism. Unfortunately, the trip didn't go as planned because of the 14 hour delay at Petersburg, VA becuase of a derailed auto-train ahead. I started crying because things didn't go as planned and I was anxious that I wasn't going to make it for Passover but people on the train calmed me down. I arrived at Deerfield Beach Friday morning at 8AM instead of Thursday afternoon at 5:45PM. My father picked me up.</p>
+            <img src="img/AmtrakAtDeerfieldBeach.jpg" alt="Amtrak at Deerfield Beach" width="250px" height="auto">
+          </div>
         </div>
     </main>
     <script src="../../js/script.js"></script>
