b9197d832e3403f0b58ce1c76951956ff2b4514d
Ephraim Becker <emb16@outlook.com>
2021-08-12 09:29:33 -0400

    Updated the sitemap.

--- college/index.php
+++ college/index.php
@@ -40,49 +40,49 @@
         <tr class="semesterDivider">
           <td>Spring 2020</td>
           <td>Fundamentals Of Computer W Micro</td>
-          <td>4</td>
+          <td style="font-weight: bold;">4</td>
           <td>A+</td>
         </tr>
         <tr class="semesterDivider">
           <td rowspan="4">Fall 2020</td>
           <td>Computing Theory And Applications</td>
-          <td>4</td>
+          <td style="font-weight: bold;">4</td>
           <td>A</td>
         </tr>
         <tr>
           <td>Introduction To Programming</td>
-          <td>3</td>
+          <td style="font-weight: bold;">3</td>
           <td>B</td>
         </tr>
         <tr>
           <td>English Composition I</td>
-          <td>3 (0)</td>
+          <td style="font-weight: bold;">0</td>
           <td>Incomplete</td>
         </tr>
         <tr>
           <td>Readings In Rambam</td>
-          <td>3</td>
+          <td style="font-weight: bold;">3</td>
           <td>C+</td>
         </tr>
         <tr class="semesterDivider">
           <td rowspan="4">Spring 2021</td>
           <td>Advanced Programming & File Struct</td>
-          <td>3</td>
+          <td style="font-weight: bold;">3</td>
           <td>A</td>
         </tr>
         <tr>
           <td>Computer Architecture</td>
-          <td>3</td>
+          <td style="font-weight: bold;">3</td>
           <td>A</td>
         </tr>
         <tr>
           <td>College Math</td>
-          <td>3</td>
+          <td style="font-weight: bold;">3</td>
           <td>A</td>
         </tr>
         <tr>
           <td>English Composition I (audit for makeup work)</td>
-          <td>3</td>
+          <td style="font-weight: bold;">3</td>
           <td>A-</td>
         </tr>
         <tr class="semesterDivider">
