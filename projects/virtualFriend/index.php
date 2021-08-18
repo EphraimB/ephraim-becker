9f30bb808491e82433300624f4ee8697bd00b71b
Ephraim Becker <emb16@outlook.com>
2021-08-18 09:01:45 -0400

    Added caninical rel tag.

--- projects/virtualFriend/index.php
+++ projects/virtualFriend/index.php
@@ -3,74 +3,19 @@
   <head>
     <meta charset="utf-8">
     <title>Ephraim Becker - Virtual Friend</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="canonical" href="https://www.ephraimbecker.com/projects/virtualFriend/" />
+    <link rel="stylesheet" href="css/style.css" />
     <link rel="icon" href="../../img/ephraim_becker.ico" type="image/x-icon" />
     <link rel="apple-touch-icon" href="../../img/ephraim-becker.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="manifest" href="/manifest.json">
-    <meta name="theme-color" media="(prefers-color-scheme: light)" content="0000FF">
-    <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000064">
-    <meta name="description" content="Hi! My name is Ephraim Becker and this is my Virtual Friend project">
-    <meta name="keywords" content="Ephraim Becker, virtual friend">
+    <meta name="theme-color" media="(prefers-color-scheme: light)" content="#0000FF" />
+    <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000064" />
+    <meta name="description" content="Hi! My name is Ephraim Becker and this is my Virtual Friend project" />
+    <meta name="keywords" content="Ephraim Becker, virtual friend" />
   </head>
   <body>
     <main>
-      <div id="register">Register</div>
-
-      <div id="closeRegistrationForm">X</div>
-
-      <div id="registrationForm">
-        <form method="post" action="php/register.php">
-          <fieldset id="profileSection">
-            <legend>Profile</legend>
-            <div id="avatar"></div>
-            <label for="avaterInput" id="avatarInputLabel">Upload photo of yourself</label>
-            <input type="file" id="avatarInput" required>
-          </fieldset>
-
-          <fieldset id="passwordSection">
-            <legend>Password</legend>
-            <input type="password" id="passwordInput" placeholder="Password" required>
-            <input type="password" id="confirmPasswordInput" placeholder="Confirm Password" required>
-          </fieldset>
-
-          <fieldset id="genderSection">
-            <legend>Gender</legend>
-
-            <label for="male" id="maleInputLabel">Male</label>
-            <input type="radio" name="gender" id="male" required>
-
-            <label for="female" id="femaleInputLabel">Female</label>
-            <input type="radio" name="gender" id="female" required>
-          </fieldset>
-
-          <input type="submit" id="submitRegistrationForm">
-        </form>
-      </div>
-
-      <div id="logIn">Log In</div>
-
-      <div id="closeLogInForm">X</div>
-
-      <div id="logInForm">
-        <form method="post" action="php/login.php">
-          <p id="formTitle">Log In</p>
-          <input type="text" id="accountIdInput" placeholder="ID" required>
-          <input type="password" id="accountPasswordInput" placeholder="Password" required>
-
-          <input type="submit" id="submitLogInForm">
-        </form>
-      </div>
-
-      <div id="account">
-
-      </div>
-      <div id="accountMenu">
-        <ul>
-          <li id="settings">Settings</li>
-          <li id="logOut">Log Out</li>
-        </ul>
-      </div>
       <div id="sky">
         <div id="star"></div>
       </div>
@@ -89,7 +34,7 @@
         <div id="personOneTalk"></div>
 
         <div id="personOneSmartphone">
-          <div id="statusBar">Verizon LTE</div>
+          <div id="statusBar">Verizon 5G</div>
           <div id="content">
           <iframe id="displayedContent" src=""></iframe>
           </div>
