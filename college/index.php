6e9f11ba1e103843788cbebe838ea7916e2b47e2
Ephraim Becker <emb16@outlook.com>
2021-08-23 17:13:37 -0400

    Added the new navbar.

--- college/index.php
+++ college/index.php
@@ -19,8 +19,13 @@
         <div id="links">
           <li><a href="../">Home</a></li>
           <li><a href="../timeline/">Timeline</a></li>
-          <li><a href="../everydayLife/">Everyday Life</a></li>
-          <li><a href="#">> College Life <</a></li>
+          <div id="dropdown">
+            <li class="focus"><a href="#" onclick="toggleNavSubmenu()">Daily Life &emsp; &#x25BC;</a></li>
+            <div id="dropdown-content">
+              <li><a href="../everydayLife/">Everyday Life</a></li>
+              <li class="focus"><a href="../college/">College Life</a></li>
+            </div>
+          </div>
           <li><a href="../resources/">Resources</a></li>
           <li><a href="../about/">About</a></li>
         </div>
--- everydayLife/accomplishments/gamingSetup/index.php
+++ everydayLife/accomplishments/gamingSetup/index.php
@@ -20,8 +20,13 @@
         <div id="links">
           <li><a href="../../../">Home</a></li>
           <li><a href="../../../timeline/">Timeline</a></li>
-          <li><a href="../../">> Everyday Life <</a></li>
-          <li><a href="../../../college/">College Life</a></li>
+          <div id="dropdown">
+            <li class="focus"><a href="#" onclick="toggleNavSubmenu()">Daily Life &emsp; &#x25BC;</a></li>
+            <div id="dropdown-content">
+              <li class="focus"><a href="../../../everydayLife/">Everyday Life</a></li>
+              <li><a href="../../../college/">College Life</a></li>
+            </div>
+          </div>
           <li><a href="../../../resources/">Resources</a></li>
           <li><a href="../../../about/">About</a></li>
         </div>
