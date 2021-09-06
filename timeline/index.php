f6f8c503bd2ffbffcc3406c7473b68375894eca6
Ephraim Becker <emb16@outlook.com>
2021-09-06 17:02:36 -0400

    Timeline doesn't show hidden events.

--- timeline/index.php
+++ timeline/index.php
@@ -55,7 +55,7 @@
       </table>
       <div itemscope itemtype="https://schema.org/Event">
         <?php
-        $sql = "SELECT * FROM timeline ORDER BY DateModified ASC";
+        $sql = "SELECT * FROM timeline WHERE hide = 0 ORDER BY DateModified ASC";
         $sqlResult = mysqli_query($link, $sql);
 
         while($row = mysqli_fetch_array($sqlResult)){
