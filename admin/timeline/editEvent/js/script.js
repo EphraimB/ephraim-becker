0a8178398dbbc81638d60c54142c1e6c3ab01f33
Ephraim Becker <emb16@outlook.com>
2021-09-14 11:04:01 -0400

    Converts to local time in SQL. DB backup.

--- admin/timeline/editEvent/js/script.js
+++ admin/timeline/editEvent/js/script.js
@@ -9,39 +9,14 @@ var timezone = document.getElementById("timezone");
 var timezoneOffset = document.getElementById("timezoneOffset");
 
 
-function msToTime(duration) {
-  var milliseconds = Math.floor(duration % 1000),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-  hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-  return hours + ":" + minutes + ":" + seconds;
-}
-
-function msToDate(duration) {
-  var date = new Date(duration);
-
-  return date.toISOString().split('T')[0];
-}
-
 document.body.onload = function formsScript() {
   if(allDay.checked) {
     eventTime.disabled = true;
-  } else {
-    var localDate = msToDate(Date.parse(eventDate.value + " " + eventTime.value) + -((timezoneOffset.value*1000)*2));
-    eventDate.value = localDate;
   }
 
   if(!endEventDateExist.checked) {
     endEventDate.disabled = true;
   }
-
-  var localTime = msToTime(Date.parse(eventDate.value + " " + eventTime.value) + -((timezoneOffset.value*1000)*2));
-  eventTime.value = localTime;
 }
 
 allDay.onchange = function() {
