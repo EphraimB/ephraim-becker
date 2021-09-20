33d0980ff8fdc9c7a37b7729dd65d48359e4d718
Ephraim Becker <emb16@outlook.com>
2021-09-20 12:11:24 -0400

    Filters the timeline into months if results are 12 or more. #32

--- timeline/filterTimeline.php
+++ timeline/filterTimeline.php
@@ -3,7 +3,46 @@
 
   global $link;
 
-  $sql = $link->prepare("SELECT *, IFNULL(DATE_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%Y-%m-%d'), EventDate) AS 'LocalDate', IFNULL(TIME_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%H:%i:%s'), NULL) AS 'LocalTime' FROM timeline WHERE hide = 0 AND YEAR(EventDate) = ? ORDER BY EventDate ASC");
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ephraim Becker - Timeline</title>
+    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="canonical" href="https://www.ephraimbecker.com/timeline/" />
+    <link rel="icon" href="../img/ephraim_becker.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="../img/ephraim-becker.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Hi! My name is Ephraim Becker and here's a timeline about my life and how people can learn from it." />
+    <meta name="keywords" content="Ephraim Becker, autism, aspergers, ADHD" />
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li id="first"><img src="../img/ephraim-becker.jpg" alt="Photo of Ephraim Becker" width="70px" height="70px" /></li>
+        <li id="hamburger-icon"><a href="#" onclick="toggleNavMenu()">&#9776;</a></li>
+        <div id="links">
+          <li><a href="../">Home</a></li>
+          <li class="focus"><a href="../timeline">Timeline</a></li>
+          <div id="dropdown">
+            <li><a href="#" onclick="toggleNavSubmenu()">Daily Life &emsp; &#x25BC;</a></li>
+            <div id="dropdown-content">
+              <li><a href="../everydayLife/">Everyday Life</a></li>
+              <li><a href="../college/">College Life</a></li>
+            </div>
+          </div>
+          <li><a href="../projects/">Projects</a></li>
+          <li><a href="../resources/">Resources</a></li>
+          <li><a href="../about/">About</a></li>
+        </div>
+      </ul>
+    </nav>
+
+<?php
+  $sql = $link->prepare("SELECT *, IFNULL(DATE_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%Y-%m-%d'), EventDate) AS 'LocalDate', IFNULL(TIME_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%H:%i:%s'), NULL) AS 'LocalTime', MONTH(EventDate) AS Month FROM timeline WHERE hide = 0 AND YEAR(EventDate) = ? ORDER BY EventDate ASC");
   $sql->bind_param("i", $year);
 
   $year = $_GET['year'];
@@ -12,48 +51,36 @@
 
   $sqlResult = $sql->get_result();
 
-  // if($sqlResult->num_rows < 12) {
-  //
-  // } else {
-  //
-  // }
-  ?>
+  if($sqlResult->num_rows > 12) {
+    $sqlTwo = $link->prepare("SELECT *, IFNULL(DATE_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%Y-%m-%d'), EventDate) AS 'LocalDate', IFNULL(TIME_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%H:%i:%s'), NULL) AS 'LocalTime', MONTH(EventDate) AS Month FROM timeline WHERE hide = 0 AND YEAR(EventDate) = ? GROUP BY Month ORDER BY EventDate ASC");
+    $sqlTwo->bind_param("i", $year);
+    $yearTwo = $_GET['year'];
 
-  <!DOCTYPE html>
-  <html lang="en" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <title>Ephraim Becker - Timeline</title>
-      <link rel="stylesheet" href="../css/style.css" />
-      <link rel="stylesheet" href="css/style.css" />
-      <link rel="canonical" href="https://www.ephraimbecker.com/timeline/" />
-      <link rel="icon" href="../img/ephraim_becker.ico" type="image/x-icon" />
-      <link rel="apple-touch-icon" href="../img/ephraim-becker.png" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Hi! My name is Ephraim Becker and here's a timeline about my life and how people can learn from it." />
-      <meta name="keywords" content="Ephraim Becker, autism, aspergers, ADHD" />
-    </head>
-    <body>
-      <nav>
-        <ul>
-          <li id="first"><img src="../img/ephraim-becker.jpg" alt="Photo of Ephraim Becker" width="70px" height="70px" /></li>
-          <li id="hamburger-icon"><a href="#" onclick="toggleNavMenu()">&#9776;</a></li>
-          <div id="links">
-            <li><a href="../">Home</a></li>
-            <li class="focus"><a href="../timeline">Timeline</a></li>
-            <div id="dropdown">
-              <li><a href="#" onclick="toggleNavSubmenu()">Daily Life &emsp; &#x25BC;</a></li>
-              <div id="dropdown-content">
-                <li><a href="../everydayLife/">Everyday Life</a></li>
-                <li><a href="../college/">College Life</a></li>
-              </div>
-            </div>
-            <li><a href="../projects/">Projects</a></li>
-            <li><a href="../resources/">Resources</a></li>
-            <li><a href="../about/">About</a></li>
-          </div>
-        </ul>
-      </nav>
+    $sqlTwo->execute();
+
+    $sqlTwoResult = $sqlTwo->get_result();
+  ?>
+      <header>
+        <h1 style="font-weight: bold;">Timeline - <?php echo $year ?> album</h1>
+      </header>
+      <main>
+      <div id="grid-container">
+        <?php
+        while($row = mysqli_fetch_array($sqlTwoResult)) {
+          $month = $row['Month'];
+
+          $dateObj = DateTime::createFromFormat('!m', $month);
+          $monthName = $dateObj->format('F'); // March
+        ?>
+        <div class="card album-cover" id="album-cover-<?php echo $year ?>-<?php echo $month ?>" onclick="filterTimeline('<?php echo $year ?>')">
+          <h2><?php echo $monthName ?></h2>
+          <p>All the events in <?php echo $monthName ?></p>
+        </div>
+      <?php } ?>
+      </div>
+  <?php
+  } else {
+  ?>
       <header>
         <h1 style="font-weight: bold;">Timeline - <?php echo $year ?> album</h1>
       </header>
@@ -87,7 +114,7 @@
         $eventMediaPortrait = $row['EventMediaPortrait'];
         $eventMediaDescription = $row['EventMediaDescription'];
         ?>
-        <div class="<?php if($memoryType == 0) { echo 'remembered-memory'; } else if($memoryType == 1) { echo 'diary-memory'; } ?> ">
+        <div style="margin-bottom: 10px;" class="<?php if($memoryType == 0) { echo 'remembered-memory'; } else if($memoryType == 1) { echo 'diary-memory'; } ?> ">
           <h2><time datetime="<?php echo $localDate ?>"><?php if(!is_null($localTime)) { echo $eventDateFormatted . " " . $eventTimeFormatted . " " . $eventTimeZone; } else { echo $eventDateFormatted; } ?></time><?php if(!is_null($endEventDate)) { echo " - <time itemprop='endDate' datetime='" . $endEventDate . "'>" . $endEventDateFormatted . "</time>"; } ?></h2>
           <h3><?php echo $eventTitle ?></h3>
           <p><?php echo $eventDescription ?></p>
@@ -110,12 +137,19 @@
         }
       ?>
     </div>
-  </main>
-</body>
-</html>
-  <?php
-  }
+    <?php
+      }
+    }
 
   $sql->close();
   $link->close();
  ?>
+
+</main>
+<footer>
+  <p>&copy; 2021 Ephraim Becker</p>
+</footer>
+<script src="../js/script.js"></script>
+<script src="js/ajax.js"></script>
+</body>
+</html>
