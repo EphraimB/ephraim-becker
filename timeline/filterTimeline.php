11c65cf6c980757dd0aecc7e5f18e6065f93c867
Ephraim Becker <emb16@outlook.com>
2021-09-20 12:59:45 -0400

    Trying to sort into days. #32

--- timeline/filterTimeline.php
+++ timeline/filterTimeline.php
@@ -163,57 +163,59 @@ if($_GET['month'] == 0) {
 
   $sqlThreeResult = $sqlThree->get_result();
 
-  while($row = mysqli_fetch_array($sqlThreeResult)) {
-    $eventTimeZone = $row['EventTimeZone'];
-    $eventTimeZoneOffset = $row['EventTimeZoneOffset'];
+  if($sqlThreeResult->num_rows < 12) {
+    while($row = mysqli_fetch_array($sqlThreeResult)) {
+      $eventTimeZone = $row['EventTimeZone'];
+      $eventTimeZoneOffset = $row['EventTimeZoneOffset'];
 
-    $eventDate = $row['EventDate'];
-    $eventTime = $row['EventTime'];
+      $eventDate = $row['EventDate'];
+      $eventTime = $row['EventTime'];
 
-    $localDate = $row['LocalDate'];
-    $localTime = $row['LocalTime'];
+      $localDate = $row['LocalDate'];
+      $localTime = $row['LocalTime'];
 
-    $endEventDate = $row['EndEventDate'];
+      $endEventDate = $row['EndEventDate'];
 
-    $endEventDateFormatted = date("F d, Y", strtotime($endEventDate));
+      $endEventDateFormatted = date("F d, Y", strtotime($endEventDate));
 
-    $eventDateFormatted = date("F d, Y", strtotime($localDate));
-    $eventTimeFormatted = date("h:i A", strtotime($localTime));
+      $eventDateFormatted = date("F d, Y", strtotime($localDate));
+      $eventTimeFormatted = date("h:i A", strtotime($localTime));
 
-    $eventTitle = $row['EventTitle'];
-    $eventDescription = $row['EventDescription'];
-    $memoryType = $row['MemoryType'];
+      $eventTitle = $row['EventTitle'];
+      $eventDescription = $row['EventDescription'];
+      $memoryType = $row['MemoryType'];
 
-    $eventYouTubeLink = $row['EventYouTubeLink'];
+      $eventYouTubeLink = $row['EventYouTubeLink'];
 
-    $eventMedia = $row['EventMedia'];
-    $eventMediaPortrait = $row['EventMediaPortrait'];
-    $eventMediaDescription = $row['EventMediaDescription'];
+      $eventMedia = $row['EventMedia'];
+      $eventMediaPortrait = $row['EventMediaPortrait'];
+      $eventMediaDescription = $row['EventMediaDescription'];
+      ?>
+      <div style="margin-bottom: 10px;" class="<?php if($memoryType == 0) { echo 'remembered-memory'; } else if($memoryType == 1) { echo 'diary-memory'; } ?> ">
+        <h2><time datetime="<?php echo $localDate ?>"><?php if(!is_null($localTime)) { echo $eventDateFormatted . " " . $eventTimeFormatted . " " . $eventTimeZone; } else { echo $eventDateFormatted; } ?></time><?php if(!is_null($endEventDate)) { echo " - <time itemprop='endDate' datetime='" . $endEventDate . "'>" . $endEventDateFormatted . "</time>"; } ?></h2>
+        <h3><?php echo $eventTitle ?></h3>
+        <p><?php echo $eventDescription ?></p>
+
+        <?php
+        if(!is_null($eventYouTubeLink)) {
+        ?>
+        <iframe width="560" height="315" src="<?php echo $eventYouTubeLink ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <?php
+        }
+
+        if(!is_null($eventMedia)) {
+          if($eventMediaPortrait == 0) {
+        ?>
+          <img src="<?php echo '../../timeline/img/' . $eventMedia ?>" alt="<?php echo $eventMediaDescription ?>" width="200px" height="113px" />
+        <?php } else { ?>
+          <img src="<?php echo '../../timeline/img/' . $eventMedia ?>" alt="<?php echo $eventMediaDescription ?>" width="113px" height="200px" />
+        <?php
+        }
+      }
     ?>
-    <div style="margin-bottom: 10px;" class="<?php if($memoryType == 0) { echo 'remembered-memory'; } else if($memoryType == 1) { echo 'diary-memory'; } ?> ">
-      <h2><time datetime="<?php echo $localDate ?>"><?php if(!is_null($localTime)) { echo $eventDateFormatted . " " . $eventTimeFormatted . " " . $eventTimeZone; } else { echo $eventDateFormatted; } ?></time><?php if(!is_null($endEventDate)) { echo " - <time itemprop='endDate' datetime='" . $endEventDate . "'>" . $endEventDateFormatted . "</time>"; } ?></h2>
-      <h3><?php echo $eventTitle ?></h3>
-      <p><?php echo $eventDescription ?></p>
-
-      <?php
-      if(!is_null($eventYouTubeLink)) {
-      ?>
-      <iframe width="560" height="315" src="<?php echo $eventYouTubeLink ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <?php
-      }
-
-      if(!is_null($eventMedia)) {
-        if($eventMediaPortrait == 0) {
-      ?>
-        <img src="<?php echo '../../timeline/img/' . $eventMedia ?>" alt="<?php echo $eventMediaDescription ?>" width="200px" height="113px" />
-      <?php } else { ?>
-        <img src="<?php echo '../../timeline/img/' . $eventMedia ?>" alt="<?php echo $eventMediaDescription ?>" width="113px" height="200px" />
-      <?php
-      }
+  </div>
+  <?php
     }
-  ?>
-</div>
-<?php
   }
 
   $sqlThree->close();
