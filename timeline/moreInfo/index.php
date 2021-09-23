34176b77d98d65c6d1c0dcbbcbee2e7d42990539
Ephraim Becker <emb16@outlook.com>
2021-09-23 15:22:19 -0400

    Added event title as title of moreInfo page. #23

--- timeline/moreInfo/index.php
+++ timeline/moreInfo/index.php
@@ -2,6 +2,46 @@
   require_once($_SERVER['DOCUMENT_ROOT'] . '/environment.php');
 
   global $link;
+
+  $sql = $link->prepare("SELECT *, IFNULL(DATE_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%Y-%m-%d'), EventDate) AS 'LocalDate', IFNULL(TIME_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%H:%i:%s'), NULL) AS 'LocalTime' FROM timeline WHERE hide=0 AND TimelineId=?");
+  $sql->bind_param("i", $id);
+
+  $id = $_GET['id'];
+
+  $sql->execute();
+
+  $sqlResult = $sql->get_result();
+
+  while($row = mysqli_fetch_array($sqlResult)) {
+    $hide = $row['hide'];
+    $timelineId = $row['TimelineId'];
+
+    $eventTimeZone = $row['EventTimeZone'];
+    $eventTimeZoneOffset = $row['EventTimeZoneOffset'];
+
+    $eventDate = $row['EventDate'];
+    $eventTime = $row['EventTime'];
+
+    $localDate = $row['LocalDate'];
+    $localTime = $row['LocalTime'];
+
+    $endEventDate = $row['EndEventDate'];
+
+    $endEventDateFormatted = date("F d, Y", strtotime($endEventDate));
+
+    $eventDateFormatted = date("F d, Y", strtotime($localDate));
+    $eventTimeFormatted = date("h:i A", strtotime($localTime));
+
+    $eventTitle = $row['EventTitle'];
+    $eventDescription = $row['EventDescription'];
+    $memoryType = $row['MemoryType'];
+
+    $eventYouTubeLink = $row['EventYouTubeLink'];
+
+    $eventMedia = $row['EventMedia'];
+    $eventMediaPortrait = $row['EventMediaPortrait'];
+    $eventMediaDescription = $row['EventMediaDescription'];
+  }
 ?>
 
 <!DOCTYPE html>
@@ -9,10 +49,10 @@
   <head>
     <meta charset="utf-8">
     <title>Ephraim Becker - Timeline - More info</title>
+    <link rel="stylesheet" href="../../css/style.css" />
     <link rel="stylesheet" href="../css/style.css" />
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="canonical" href="https://www.ephraimbecker.com/timeline/" />
-    <link rel="icon" href="../img/ephraim_becker.ico" type="image/x-icon" />
+    <link rel="canonical" href="https://www.ephraimbecker.com/timeline/moreInfo/" />
+    <link rel="icon" href="../../img/ephraim_becker.ico" type="image/x-icon" />
     <link rel="apple-touch-icon" href="../img/ephraim-becker.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Hi! My name is Ephraim Becker and here's a timeline about my life and how people can learn from it." />
@@ -21,70 +61,28 @@
   <body>
     <nav>
       <ul>
-        <li id="first"><img src="../img/ephraim-becker.jpg" alt="Photo of Ephraim Becker" width="70px" height="70px" /></li>
+        <li id="first"><img src="../../img/ephraim-becker.jpg" alt="Photo of Ephraim Becker" width="70px" height="70px" /></li>
         <li id="hamburger-icon"><a href="#" onclick="toggleNavMenu()">&#9776;</a></li>
         <div id="links">
-          <li><a href="../">Home</a></li>
-          <li class="focus"><a href="../">Timeline - More info</a></li>
+          <li><a href="../../">Home</a></li>
+          <li class="focus"><a href="../">Timeline</a></li>
           <div id="dropdown">
             <li><a href="#" onclick="toggleNavSubmenu()">Daily Life &emsp; &#x25BC;</a></li>
             <div id="dropdown-content">
-              <li><a href="../everydayLife/">Everyday Life</a></li>
-              <li><a href="../college/">College Life</a></li>
+              <li><a href="../../everydayLife/">Everyday Life</a></li>
+              <li><a href="../../college/">College Life</a></li>
             </div>
           </div>
-          <li><a href="../projects/">Projects</a></li>
-          <li><a href="../resources/">Resources</a></li>
-          <li><a href="../about/">About</a></li>
+          <li><a href="../../projects/">Projects</a></li>
+          <li><a href="../../resources/">Resources</a></li>
+          <li><a href="../../about/">About</a></li>
         </div>
       </ul>
     </nav>
     <header>
-      <h1 style="font-weight: bold;">Timeline</h1>
+      <h1 style="font-weight: bold;">Timeline - <?php echo $eventTitle ?></h1>
     </header>
     <main>
-      <?php
-      $sql = $link->prepare("SELECT *, IFNULL(DATE_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%Y-%m-%d'), EventDate) AS 'LocalDate', IFNULL(TIME_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%H:%i:%s'), NULL) AS 'LocalTime' FROM timeline WHERE hide=0 TimelineId=?");
-      $sql->bind_param("i", $id);
-
-      $id = $_GET['id'];
-
-      $sql->execute();
-
-      $sqlResult = $sql->get_result();
-
-      while($row = mysqli_fetch_array($sqlResult)) {
-        $hide = $row['hide'];
-        $timelineId = $row['TimelineId'];
-
-        $eventTimeZone = $row['EventTimeZone'];
-        $eventTimeZoneOffset = $row['EventTimeZoneOffset'];
-
-        $eventDate = $row['EventDate'];
-        $eventTime = $row['EventTime'];
-
-        $localDate = $row['LocalDate'];
-        $localTime = $row['LocalTime'];
-
-        $endEventDate = $row['EndEventDate'];
-
-        $endEventDateFormatted = date("F d, Y", strtotime($endEventDate));
-
-        $eventDateFormatted = date("F d, Y", strtotime($localDate));
-        $eventTimeFormatted = date("h:i A", strtotime($localTime));
-
-        $eventTitle = $row['EventTitle'];
-        $eventDescription = $row['EventDescription'];
-        $memoryType = $row['MemoryType'];
-
-        $eventYouTubeLink = $row['EventYouTubeLink'];
-
-        $eventMedia = $row['EventMedia'];
-        $eventMediaPortrait = $row['EventMediaPortrait'];
-        $eventMediaDescription = $row['EventMediaDescription'];
-      }
-      ?>
-
       <div class="<?php if($hide == 1) { echo 'hidden-memory '; } if($memoryType == 0) { echo 'remembered-memory'; } else if($memoryType == 1) { echo 'diary-memory'; } ?> ">
         <h2><time itemprop="startDate" datetime="<?php echo $localDate ?>"><?php if(!is_null($localTime)) { echo $eventDateFormatted . " " . $eventTimeFormatted . " " . $eventTimeZone; } else { echo $eventDateFormatted; } ?></time><?php if(!is_null($endEventDate)) { echo " - <time itemprop='endDate' datetime='" . $endEventDate . "'>" . $endEventDateFormatted . "</time>"; } ?></h2>
         <h3 itemprop="name"><?php echo $eventTitle ?></h3>
