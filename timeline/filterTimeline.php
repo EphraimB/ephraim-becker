3308530a3e4fdee6226587958d434f74f6993546
Ephraim Becker <emb16@outlook.com>
2021-10-01 09:49:03 -0400

    The next nav button is hidden on last event. #46

--- timeline/filterTimeline.php
+++ timeline/filterTimeline.php
@@ -99,37 +99,39 @@
 
     $sqlDatesResult = $sqlDates->get_result();
 
-    while($row = mysqli_fetch_array($sqlDatesResult)) {
-      $nextYear = $row['Year'];
-      $nextMonth = $row['Month'];
-      $nextDay = $row['Day'];
-    }
-
-    if($month > 0 && $day > 0) {
-      $year = $nextYear;
-      $month = $nextMonth;
-      $day = $nextDay;
-    } else if($month == 0 && $day == 0) {
-      $sqlDatesTwo = $link->prepare("SELECT EventDate, YEAR(EventDate) AS Year, MONTH(EventDate) AS Month, DAY(EventDate) AS Day FROM timeline WHERE EventDate>? GROUP BY Year LIMIT 1");
-      $sqlDatesTwo->bind_param("s", $navvedEventDate);
-
-      $navvedEventDate = $nextYear . "-12-31";
-
-      $sqlDatesTwo->execute();
-
-      $sqlDatesTwoResult = $sqlDatesTwo->get_result();
-
-      while($rowTwo = mysqli_fetch_array($sqlDatesTwoResult)) {
-        $year = $rowTwo['Year'];
+    if($sqlDatesResult->num_rows > 0) {
+      while($row = mysqli_fetch_array($sqlDatesResult)) {
+        $nextYear = $row['Year'];
+        $nextMonth = $row['Month'];
+        $nextDay = $row['Day'];
       }
-    }
 
-    ?>
-    <div class="row navButton" onclick="filterTimeline('<?php echo $year ?>', '<?php echo $month ?>', '<?php echo $day ?>')">
-       >
-    </div>
-    <?php
+      if($month > 0 && $day > 0) {
+        $year = $nextYear;
+        $month = $nextMonth;
+        $day = $nextDay;
+      } else if($month == 0 && $day == 0) {
+        $sqlDatesTwo = $link->prepare("SELECT EventDate, YEAR(EventDate) AS Year, MONTH(EventDate) AS Month, DAY(EventDate) AS Day FROM timeline WHERE EventDate>? GROUP BY Year LIMIT 1");
+        $sqlDatesTwo->bind_param("s", $navvedEventDate);
+
+        $navvedEventDate = $nextYear . "-12-31";
+
+        $sqlDatesTwo->execute();
+
+        $sqlDatesTwoResult = $sqlDatesTwo->get_result();
+
+        while($rowTwo = mysqli_fetch_array($sqlDatesTwoResult)) {
+          $year = $rowTwo['Year'];
+        }
+      }
+
+      ?>
+      <div class="row navButton" onclick="filterTimeline('<?php echo $year ?>', '<?php echo $month ?>', '<?php echo $day ?>')">
+         >
+      </div>
+      <?php
     }
+  }
 
   function displaySorter($sqlResult) {
     $year = $_GET['year'];
