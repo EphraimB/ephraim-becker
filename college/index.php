ccb0fa7ecb79f9c0eb7ee903432e1e85e5de80bb
Ephraim Becker <emb16@outlook.com>
2021-10-10 12:09:26 -0400

    Optimized the college page. #54

--- college/index.php
+++ college/index.php
@@ -1,42 +1,10 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Ephraim Becker - College</title>
-    <link rel="stylesheet" href="../css/style.css" />
-    <link rel="canonical" href="https://www.ephraimbecker.com/college/" />
-    <link rel="icon" href="../img/ephraim_becker.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="../img/ephraim-becker.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="Hi! My name is Ephraim Becker and this is how I'm doing in college." />
-    <meta name="keywords" content="Ephraim Becker, autism, aspergers, ADHD" />
-  </head>
-  <body>
-    <nav>
-      <ul>
-        <li id="first"><img src="../img/ephraim-becker.jpg" alt="Photo of Ephraim Becker" width="70px" height="70px" /></li>
-        <li id="hamburger-icon"><a href="#" onclick="toggleNavMenu()">&#9776;</a></li>
-        <div id="links">
-          <li><a href="../">Home</a></li>
-          <li><a href="../timeline/">Timeline</a></li>
-          <div id="dropdown">
-            <li class="focus"><a href="#" onclick="toggleNavSubmenu()">Daily Life &emsp; &#x25BC;</a></li>
-            <div id="dropdown-content">
-              <li><a href="../everydayLife/">Everyday Life</a></li>
-              <li class="focus"><a href="../college/">College Life</a></li>
-            </div>
-          </div>
-          <li><a href="../projects/">Projects</a></li>
-          <li><a href="../resources/">Resources</a></li>
-          <li><a href="../about/">About</a></li>
-        </div>
-      </ul>
-    </nav>
-    <header>
-      <h1 style="font-weight: bold;">College Life</h1>
-    </header>
-    <main>
-      <table>
+<?php
+  $title = "Ephraim Becker - College";
+  $header = "College Life";
+  $localStyleSheet = NULL;
+  $localScript = NULL;
+
+  $body = '<table>
         <caption>Touro College - BS in Computer Science</caption>
         <tr>
           <th>Semester</th>
@@ -131,8 +99,7 @@
       <div>
         <label for="DegreeProgress">Degree progress:</label>
         <progress id="DegreeProgress" value="26" max="120">21.7%</progress>
-      </div>
-    </main>
-    <script src="../js/script.js"></script>
-  </body>
-</html>
+      </div>';
+
+  require("../base.php");
+?>
