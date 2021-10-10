122ed1196277c5b7abad56d7bcfc51b0c31384dd
Ephraim Becker <emb16@outlook.com>
2021-10-10 14:40:05 -0400

    Optimized the edit event page. #54

--- admin/timeline/editEvent/index.php
+++ admin/timeline/editEvent/index.php
@@ -6,37 +6,15 @@
   $year = $_GET['year'];
   $month = $_GET['month'];
   $day = $_GET['day'];
-?>
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Ephraim Becker - Admin - Timeline - Edit Event</title>
-    <link rel="stylesheet" href="../../../css/style.css" />
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="icon" href="../../../img/ephraim_becker.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="../../../img/ephraim-becker.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  </head>
-  <body>
-    <nav>
-      <ul>
-        <li id="first"><img src="../../../img/ephraim-becker.jpg" alt="Photo of Ephraim Becker" width="70px" height="70px" /></li>
-        <li id="hamburger-icon"><a href="#" onclick="toggleNavMenu()">&#9776;</a></li>
-        <div id="links">
-          <li><a href="../../">Admin</a></li>
-          <li class="focus"><a href="../">Timeline</a></li>
-        </div>
-      </ul>
-    </nav>
-    <header>
-      <h1 style="font-weight: bold;">Admin - Timeline - Edit Event</h1>
-    </header>
-    <main>
-      <form action="editEvent.php" method="post" enctype="multipart/form-data">
-        <div class="row">
-          <?php
+  $title = "Ephraim Becker - Admin - Timeline - Edit Event";
+  $header = "Admin - Timeline - Edit Event";
+  $localStyleSheet = '<link rel="stylesheet" href="css/style.css" />';
+  $localScript = '<script src="js/script.js"></script>';
+
+  $body = '<form action="editEvent.php" method="post" enctype="multipart/form-data">
+        <div class="row">';
+
           $sql = $link->prepare("SELECT *, IFNULL(DATE_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%Y-%m-%d'), EventDate) AS 'LocalDate', IFNULL(TIME_FORMAT(concat(EventDate, ' ', EventTime) - INTERVAL EventTimeZoneOffset SECOND, '%H:%i:%s'), NULL) AS 'LocalTime' FROM timeline WHERE TimelineId=?");
           $sql->bind_param("i", $id);
 
@@ -69,21 +47,25 @@
             $eventMedia = $row['EventMedia'];
             $eventMediaDescription = $row['EventMediaDescription'];
           }
-          ?>
-          <div>
+
+          $body .= '<div>
             <label for="eventDate">Start event date:</label>
             <br />
-            <input type="date" id="eventDate" name="eventDate" value="<?php echo $localDate ?>" required />
+            <input type="date" id="eventDate" name="eventDate" value="' . $localDate . '" required />
           </div>
           <div>
             <label for="eventTime">Event time (optional):</label>
             <br />
-            <input type="time" id="eventTime" name="eventTime" value="<?php echo $localTime ?>" />
-            <input type="hidden" id="timezone" name="timezone" value="<?php echo $eventTimeZone ?>" />
-            <input type="hidden" id="timezoneOffset" name="timezoneOffset" value="<?php echo $eventTimeZoneOffset ?>" />
+            <input type="time" id="eventTime" name="eventTime" value="' . $localTime . '" />
+            <input type="hidden" id="timezone" name="timezone" value="' . $eventTimeZone . '" />
+            <input type="hidden" id="timezoneOffset" name="timezoneOffset" value="' . $eventTimeZoneOffset . '" />
           </div>
           <div>
-            <input type="checkbox" id="allDay" name="allDay" value="allDay" <?php if(is_null($eventTime)) { echo "checked"; } ?>>
+            <input type="checkbox" id="allDay" name="allDay" value="allDay" ';
+            if(is_null($eventTime)) {
+              $body .= "checked";
+            }
+            $body .= '>
             <label for="allDay">allDay?</label>
           </div>
         </div>
@@ -92,10 +74,15 @@
           <div>
             <label for="endEventDate">End event date:</label>
             <br />
-            <input type="date" id="endEventDate" name="endEventDate" value="<?php echo $endEventDate ?>" />
+            <input type="date" id="endEventDate" name="endEventDate" value="' . $endEventDate . '" />
           </div>
           <div>
-            <input type="checkbox" id="endEventDateExist" name="endEventDateExist" value="endEventDateExist" <?php if(!is_null($endEventDate)) { echo "checked"; } ?>>
+            <input type="checkbox" id="endEventDateExist" name="endEventDateExist" value="endEventDateExist" ';
+
+            if(!is_null($endEventDate)) {
+              $body = "checked";
+            }
+            $body .= '>
             <label for="endEventDateExist">End event date exist?</label>
           </div>
         </div>
@@ -103,13 +90,13 @@
         <div>
           <label for="eventTitle">Event title:</label />
           <br />
-          <input type="text" id="eventTitle" name="eventTitle" value="<?php echo $eventTitle ?>" required />
+          <input type="text" id="eventTitle" name="eventTitle" value="' . $eventTitle . '" required />
         </div>
         <br />
         <div>
           <label for="eventDescription">Event description:</label>
           <br />
-          <textarea id="eventDescription" name="eventDescription" rows="6" cols="50" required><?php echo $eventDescription ?></textarea>
+          <textarea id="eventDescription" name="eventDescription" rows="6" cols="50" required>' . $eventDescription . '</textarea>
         </div>
         <br />
         <div>
@@ -127,40 +114,51 @@
         <div>
           <label for="eventImage">YouTube link:</label>
           <br />
-          <input id="eventYouTubeLink" name="eventYouTubeLink" type="text" value="<?php echo $eventYouTubeLink ?>" />
+          <input id="eventYouTubeLink" name="eventYouTubeLink" type="text" value="' . $eventYouTubeLink . '" />
         </div>
         <div>
           <h3>Event memory type:</h3>
           <div class="row">
             <div class="remembered-memory">
-              <input type="radio" id="remembered" name="memory" value="0" <?php if($memoryType == 0){ echo "checked"; } ?> required />
+              <input type="radio" id="remembered" name="memory" value="0" ';
+
+              if($memoryType == 0) {
+                $body .= "checked";
+              }
+              $body .= ' required />
               <label for="remembered">Remembered memory</label>
             </div>
             <div class="diary-memory">
-              <input type="radio" id="diary" name="memory" value="1" <?php if($memoryType == 1){ echo "checked"; } ?> />
+              <input type="radio" id="diary" name="memory" value="1" ';
+
+              if($memoryType == 1) {
+                $body .= "checked";
+               }
+               $body .= ' />
               <label for="diary">Diary memory</label>
             </div>
             <div class="hidden-memory remembered-memory">
-              <input type="checkbox" id="hidden" name="hidden" value="1" <?php if($hide == 1) { echo "checked"; } ?> />
+              <input type="checkbox" id="hidden" name="hidden" value="1" ';
+
+              if($hide == 1) {
+                $body .= "checked";
+               }
+               $body .= ' />
               <label for="hidden">Hidden memory</label>
             </div>
           </div>
         </div>
-        <input type="hidden" name="id" value="<?php echo $timelineId ?>" />
-        <input type="hidden" name="year" value="<?php echo $year ?>" />
-        <input type="hidden" name="month" value="<?php echo $month ?>" />
-        <input type="hidden" name="day" value="<?php echo $day ?>" />
+        <input type="hidden" name="id" value="' . $timelineId . '" />
+        <input type="hidden" name="year" value="' . $year . '" />
+        <input type="hidden" name="month" value="' . $month . '" />
+        <input type="hidden" name="day" value="' . $day . '" />
         <br />
         <input type="submit" id="submit" value="Edit event" />
         <br />
-      </form>
-    </main>
-    <script src="../../../js/script.js"></script>
-    <script src="js/script.js"></script>
-  </body>
-</html>
+      </form>';
 
-<?php
   $sql->close();
   $link->close();
+
+  require("../../base.php");
  ?>
