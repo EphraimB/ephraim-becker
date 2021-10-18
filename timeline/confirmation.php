1d84b43822ab46e391b8a9c8add6e7c49b2c24d6
Ephraim Becker <emb16@outlook.com>
2021-10-17 21:14:18 -0400

    Added the editEvent folder and hide functions. #57

---
+++ timeline/confirmation.php
@@ -0,0 +1,37 @@
+<?php
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/environment.php');
+
+  global $link;
+
+  $id = $_GET['id'];
+
+  $title = "Ephraim Becker - Admin - Timeline - Delete?";
+  $header = "Admin - Timeline - Delete?";
+  $localStyleSheet = '<link rel="stylesheet" href="css/style.css" />';
+  $localScript = NULL;
+
+        $sql = $link->prepare("SELECT EventTitle FROM timeline WHERE TimelineId=?");
+        $sql->bind_param("i", $id);
+
+        $id = $_GET['id'];
+
+        $sql->execute();
+
+        $sqlResult = $sql->get_result();
+
+        while($row = mysqli_fetch_array($sqlResult)){
+          $eventTitle = $row['EventTitle'];
+        }
+
+      $body = '<h2>Are you sure you want to delete the event named "' . $eventTitle . '"?</h2>
+
+      <div class="row actionButtons">
+        <a class="keep" href="index.php">No</a>
+        <a class="delete" href="deleteEvent.php?id=' . $id . '">Yes</a>
+      </div>
+    </main>
+  </body>
+</html>';
+
+require("../base.php");
+?>
---
+++ timeline/unhideEvent.php
@@ -0,0 +1,22 @@
+<?php
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/environment.php');
+
+  global $link;
+
+
+  $sql = $link->prepare("UPDATE timeline SET hide = 0 WHERE TimelineId=?");
+  $sql->bind_param("i", $id);
+
+  $id = $_GET['id'];
+
+  $sql->execute();
+
+  $sql->close();
+  $link->close();
+
+  $year = $_GET['year'];
+  $month = $_GET['month'];
+  $day = $_GET['day'];
+
+  header("location: index.php#" . $year . "-" . $month . "-" . $day);
+?>
