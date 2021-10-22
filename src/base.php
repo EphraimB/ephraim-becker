478fd282606170a1405a08f147eca9078bd72b50
Ephraim Becker <emb16@outlook.com>
2021-10-21 21:47:36 -0400

    Simplified some code. #62

--- src/base.php
+++ src/base.php
@@ -25,9 +25,39 @@ final class Base
     return $this->documentRoot;
   }
 
-  function ensureValidDocumentRoot(string $documentRoot): bool
+  function getLocalStyleSheet()
   {
-    if($documentRoot == '/') {
+    return $this->localStyleSheet;
+  }
+
+  function getUrl(): string
+  {
+    return $this->url;
+  }
+
+  function getTitle(): string
+  {
+    return $this->title;
+  }
+
+  function getHeader()
+  {
+    return $this->header;
+  }
+
+  function getBody(): string
+  {
+    return $this->body;
+  }
+
+  function getLocalScript()
+  {
+    return $this->localScript;
+  }
+
+  function ensureValidDocumentRoot(): bool
+  {
+    if($this->getDocumentRoot() == '/') {
       return true;
     } else {
       return false;
@@ -41,10 +71,10 @@ final class Base
         <meta charset="utf-8">
         <title>' . $title . '</title>
         <link rel="stylesheet" href="' . $this->getDocumentRoot() . '/css/style.css" />
-        ' . $localStyleSheet . '
+        ' . $this->getLocalStyleSheet() . '
         <link rel="canonical" href="https://www.ephraimbecker.com/" />
-        <link rel="icon" href="' . $documentRoot . '/img/ephraim_becker.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="' . $documentRoot . '/img/ephraim-becker.png" />
+        <link rel="icon" href="' . $this->getDocumentRoot() . '/img/ephraim_becker.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="' . $this->getDocumentRoot() . '/img/ephraim-becker.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Hi! My name is Ephraim Becker and this is a website about my life and how people can learn from it." />
         <meta name="keywords" content="Ephraim Becker, autism, aspergers, ADHD" />
@@ -54,31 +84,31 @@ final class Base
     return $html;
   }
 
-  function nav(string $documentRoot, string $url): string
+  function nav(): string
   {
     $html = '
       <nav>
         <ul>
-          <li id="first"><img src="' . $documentRoot . '/img/ephraim-becker.jpg" alt="Photo of Ephraim Becker" width="70px" height="70px" /></li>
+          <li id="first"><img src="' . $this->getDocumentRoot() . '/img/ephraim-becker.jpg" alt="Photo of Ephraim Becker" width="70px" height="70px" /></li>
           <li id="hamburger-icon"><a href="javascript:;" onclick="toggleNavMenu()">&#9776;</a></li>
           <div id="links">
-            <li><a href="' . $documentRoot . '/index.php">Home</a></li>
-            <li><a href="' . $documentRoot . '/timeline/">Timeline</a></li>
+            <li><a href="' . $this->getDocumentRoot() . '/index.php">Home</a></li>
+            <li><a href="' . $this->getDocumentRoot() . '/timeline/">Timeline</a></li>
             <div id="dropdown">
               <li><a href="javascript:;" onclick="toggleNavSubmenu()">Daily Life &emsp; &#x25BC;</a></li>
               <div id="dropdown-content">
-                <li><a href="' . $documentRoot . '/everydayLife/">Everyday Life</a></li>
-                <li><a href="' . $documentRoot . '/college/">College Life</a></li>
+                <li><a href="' . $this->getDocumentRoot() . '/everydayLife/">Everyday Life</a></li>
+                <li><a href="' . $this->getDocumentRoot() . '/college/">College Life</a></li>
               </div>
             </div>
-            <li><a href="' . $documentRoot . '/projects/">Projects</a></li>
-            <li><a href="' . $documentRoot . '/resources/">Resources</a></li>
-            <li><a href="' . $documentRoot . '/about/">About</a></li>';
+            <li><a href="' . $this->getDocumentRoot() . '/projects/">Projects</a></li>
+            <li><a href="' . $this->getDocumentRoot() . '/resources/">Resources</a></li>
+            <li><a href="' . $this->getDocumentRoot() . '/about/">About</a></li>';
 
             if(isset($_SESSION['username'])) {
-              $html .= '<li><a href="' . $documentRoot . '/adminLogout.php?fromUrl=' . $url . '">Logout</a></li>';
+              $html .= '<li><a href="' . $this->getDocumentRoot() . '/adminLogout.php?fromUrl=' . $this->getUrl() . '">Logout</a></li>';
             } else {
-              $html .= '<li><a href="' . $documentRoot . '/adminLogin/index.php?fromUrl=' . $url . '">Login</a></li>';
+              $html .= '<li><a href="' . $this->getDocumentRoot() . '/adminLogin/index.php?fromUrl=' . $this->getUrl() . '">Login</a></li>';
             }
           $html .= '</div>
         </ul>
@@ -87,20 +117,20 @@ final class Base
       return $html;
     }
 
-    function header($header): string
+    function header(): string
     {
       $html = '<header>
-        <h1 style="font-weight: bold;">' . $header . '</h1>
+        <h1 style="font-weight: bold;">' . $this->getHeader() . '</h1>
       </header>';
 
       return $html;
     }
 
-    function main(string $body): string
+    function main(): string
     {
       $html = '
         <main id="main">
-          ' . $body . '
+          ' . $this->getBody() . '
         </main>';
 
       return $html;
@@ -115,25 +145,25 @@ final class Base
       return $html;
     }
 
-    function scripts($localScript, string $documentRoot): string
+    function scripts(): string
     {
-      $html = '<script src="' . $documentRoot . '/js/script.js"></script>
-      ' . $localScript;
+      $html = '<script src="' . $this->getDocumentRoot() . '/js/script.js"></script>
+      ' . $this->getLocalScript();
 
       return $html;
     }
 
-    function html($localStyleSheet, string $url, string $title, $header, string $body, $localScript, string $documentRoot)
+    function html()
     {
       $html = '<!DOCTYPE html>
       <html lang="en" dir="ltr">';
-      $html .= $this->head($localStyleSheet, $title);
+      $html .= $this->head($this->getLocalStyleSheet(), $this->getTitle());
       $html .= '<body>';
-      $html .= $this->nav($documentRoot, $url);
-      $html .= $this->header($header);
-      $html .= $this->main($body);
+      $html .= $this->nav($this->getUrl());
+      $html .= $this->header($this->getHeader());
+      $html .= $this->main($this->getBody());
       $html .= $this->footer();
-      $html .= $this->scripts($localScript, $documentRoot);
+      $html .= $this->scripts($this->getLocalScript());
       $html .= '
         </body>
       </html>';
@@ -143,6 +173,5 @@ final class Base
 }
 
 $base = new Base($localStyleSheet, $url, $title, $header, $body, $localScript);
-$documentRoot = $base->getDocumentRoot();
-$base->html($localStyleSheet, $url, $title, $header, $body, $localScript, $documentRoot);
+$base->html();
 ?>
