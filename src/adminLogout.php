67646b881784688ef20d435f2ee22859bd12f77a
Ephraim Becker <emb16@outlook.com>
2021-11-17 10:35:30 -0500

    Refactored the logout page into a class.

--- src/adminLogout.php
+++ src/adminLogout.php
@@ -1,9 +1,35 @@
 <?php
-  session_start();
+declare(strict_types=1);
 
-  unset($_SESSION['username']);
+session_start();
 
-  $url = $_GET['fromUrl'];
+class Logout
+{
+  private $url;
 
-  header("location: " . $url);
-?>
+  function __construct()
+  {
+
+  }
+
+  function setUrl($url): void
+  {
+    $this->url = $url;
+  }
+
+  function getUrl(): string
+  {
+    return $this->url;
+  }
+
+  function logout(): void
+  {
+    unset($_SESSION['username']);
+
+    header("location: " . $this->getUrl());
+  }
+}
+
+$logout = new Logout();
+$logout->setUrl($_GET['fromUrl']);
+$logout->logout();
