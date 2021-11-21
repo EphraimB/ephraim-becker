aa0b5908f93bf14e2fc1e4d9e5ce8b2af611d1d3
Ephraim Becker <emb16@outlook.com>
2021-11-21 11:40:35 -0500

    I can now delete a thought again.

--- src/timeline/moreInfo/editThought/editThought.php
+++ src/timeline/moreInfo/editThought/editThought.php
@@ -1,47 +1,125 @@
 <?php
-  session_start();
+declare(strict_types=1);
 
-  require_once($_SERVER['DOCUMENT_ROOT'] . '/environment.php');
+session_start();
 
-  global $link;
+require_once($_SERVER['DOCUMENT_ROOT'] . '/environment.php');
 
-  if(!isset($_SESSION['username'])) {
-    header("location: ../");
-  }
-  
-  $sql = $link->prepare("UPDATE thoughts SET DateModified=?, Thought=?, hide=? WHERE ThoughtId=?");
-  $sql->bind_param('ssii', $dateModified, $thought, $hidden, $id);
+class EditThought
+{
+  private $isAdmin;
+  private $link;
+  private $id;
+  private $thought;
+  private $hidden;
 
-  $id = $_POST['id'];
+  function __construct()
+  {
+    $this->setIsAdmin();
 
-  $dateModified = date("Y-m-d H:i:s");
-  $thought = $_POST['thought'];
-
-  if(empty($_POST['hidden'])) {
-    $hidden = 0;
-  } else {
-    $hidden = $_POST['hidden'];
-  }
-
-
-  $sql->execute();
-
-  $sql->close();
-
-  $sqlTwo = $link->prepare("SELECT TimelineId FROM thoughts WHERE ThoughtId=? LIMIT 1");
-
-  $sqlTwo->bind_param("i", $id);
-
-  $sqlTwo->execute();
-
-  $sqlTwoResult = $sqlTwo->get_result();
-
-  while($row = mysqli_fetch_array($sqlTwoResult)) {
-    $timelineId = $row['TimelineId'];
+    if(!$this->getIsAdmin()) {
+      header("location: ../");
+    }
   }
 
-  $sqlTwo->close();
-  $link->close();
+  function setIsAdmin(): void
+  {
+    if(isset($_SESSION['username'])) {
+      $this->isAdmin = true;
+    } else {
+      $this->isAdmin = false;
+    }
+  }
 
-  header("location: ../index.php?id=". $timelineId);
+  function getIsAdmin(): bool
+  {
+    return $this->isAdmin;
+  }
+
+  function setLink($link)
+  {
+    $this->link = $link;
+  }
+
+  function getLink()
+  {
+    return $this->link;
+  }
+
+  function setId($id): void
+  {
+    $this->id = $id;
+  }
+
+  function getId(): int
+  {
+    return intval($this->id);
+  }
+
+  function setThought($thought): void
+  {
+    $this->thought = $thought;
+  }
+
+  function getThought(): string
+  {
+    return $this->thought;
+  }
+
+  function setHidden($hidden): void
+  {
+    $this->hidden = $hidden;
+  }
+
+  function getHidden(): int
+  {
+    return $this->hidden;
+  }
+
+  function editThought(): void
+  {
+    $sql = $this->getLink()->prepare("UPDATE thoughts SET DateModified=?, Thought=?, hide=? WHERE ThoughtId=?");
+    $sql->bind_param('ssii', $dateModified, $thought, $hidden, $id);
+
+    $id = $this->getId();
+
+    $dateModified = date("Y-m-d H:i:s");
+    $thought = $this->getThought();
+    $hidden = $this->getHidden();
+
+    $sql->execute();
+
+    $sql->close();
+
+    $sqlTwo = $this->getLink()->prepare("SELECT TimelineId FROM thoughts WHERE ThoughtId=? LIMIT 1");
+
+    $sqlTwo->bind_param("i", $id);
+
+    $sqlTwo->execute();
+
+    $sqlTwoResult = $sqlTwo->get_result();
+
+    while($row = mysqli_fetch_array($sqlTwoResult)) {
+      $timelineId = $row['TimelineId'];
+    }
+
+    header("location: ../index.php?id=". $timelineId);
+  }
+}
+$config = new Config();
+$link = $config->connectToServer();
+
+$editThought = new EditThought();
+$editThought->setLink($link);
+$editThought->setId($_POST['id']);
+$editThought->setThought($_POST['thought']);
+
+if(empty($_POST['hidden'])) {
+  $hidden = 0;
+} else {
+  $hidden = $_POST['hidden'];
+}
+$editThought->setHidden(intval($hidden));
+
+$editThought->editThought();
 ?>
