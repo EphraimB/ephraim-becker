a2dc77a71f5712b3a273e65ac27f714bb2e4a57d
Ephraim Becker <emb16@outlook.com>
2021-11-25 19:53:33 -0500

    Laying out the groundwork for the goals database. #70

--- src/everydayLife/goals/index.php
+++ src/everydayLife/goals/index.php
@@ -4,48 +4,177 @@ declare(strict_types=1);
 session_start();
 
 require($_SERVER['DOCUMENT_ROOT'] . "/base.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . '/environment.php');
 
 class Goals extends Base
 {
+  private $link;
+  private $isAdmin;
 
   function __construct()
   {
 
   }
 
+  function setIsAdmin(): void
+  {
+    if(isset($_SESSION['username'])) {
+      $this->isAdmin = true;
+    } else {
+      $this->isAdmin = false;
+    }
+  }
+
+  function getIsAdmin(): bool
+  {
+    return $this->isAdmin;
+  }
+
+  function setLink($link)
+  {
+    $this->link = $link;
+  }
+
+  function getLink()
+  {
+    return $this->link;
+  }
+
+  function addGoal(): string
+  {
+    if($this->getIsAdmin()) {
+      $html = '<div class="row">
+            <ul class="subNav">
+              <li><a style="text-decoration: none;" href="addGoal/">+</a></li>
+            </ul>
+          </div>';
+      } else {
+        $html = '';
+      }
+
+    return $html;
+  }
+
+  function goals(): string
+  {
+    $sql = "SELECT * FROM goals";
+    $sqlResult = mysqli_query($this->getLink(), $sql);
+
+    $body = '
+    <table>
+      <tr>
+        <th>Goal</th>
+        <th>How it will be done</th>';
+
+        if($this->getIsAdmin()) {
+          $body .= '<th>Actions</th>';
+        }
+
+      $body .= '</tr>';
+
+      while($row = mysqli_fetch_array($sqlResult)) {
+        $goalId = $row['GoalId'];
+        $goal = $row['Goal'];
+        $how = $row['How'];
+
+        $body .= '
+        <tr>
+          <td>' . $goal . '</td>
+          <td>' . $how . '</td>';
+
+          if($this->getIsAdmin()) {
+            $body .= '
+              <td class="actionButtons">
+                <a class="edit" href="editGoal/index.php?id=' . $id . '">Edit</a>
+                <a class="delete" href="confirmationGoal.php?id=' . $id . '">Delete</a>
+              </td>';
+          }
+        $body .= '</tr>';
+      }
+
+      $body .= '</table>';
+
+    return $body;
+  }
+
+  function addGoalLog(): string
+  {
+    if($this->getIsAdmin()) {
+      $html = '<div class="row">
+            <ul class="subNav">
+              <li><a style="text-decoration: none;" href="addGoalLog/">+</a></li>
+            </ul>
+          </div>';
+      } else {
+        $html = '';
+      }
+
+    return $html;
+  }
+
+  function goalLog(): string
+  {
+    $body = '<h2>Goal log</h2>
+    <table>
+      <tr>
+        <th>Date</th>
+        <th>Goal worked on</th>
+        <th>Log</th>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>';
+
+    return $body;
+  }
+
   function main(): string
   {
-      $body = '<ol style="text-align: left;">
-          <li>Get out of defense mode (hopefully with some course in <a target="_blank" rel="noopener" href="https://www.aspergerexperts.com/">Aspergers Experts</a>)</li>
-          <li>Become better at socializing</li>
-          <li>Expressing myself more</li>
-          <li>Mindfulness</li>
-          <li>Get out of the house and be independent</li>
-          <li>Being more professional in the workplace</li>
-        </ol>
+    $body = $this->addGoal();
+    $body .= $this->goals();
+    $body .= '<br />';
+    $body .= $this->addGoalLog();
+    $body .= $this->goalLog();
 
-        <h2>Goal log</h2>
-        <table>
-          <tr>
-            <th>Date</th>
-            <th>Goal worked on</th>
-            <th>Log</th>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </table>';
+    // $body = '<ol style="text-align: left;">
+      //   <li>Get out of defense mode (hopefully with some course in <a target="_blank" rel="noopener" href="https://www.aspergerexperts.com/">Aspergers Experts</a>)</li>
+      //   <li>Become better at socializing</li>
+      //   <li>Expressing myself more</li>
+      //   <li>Mindfulness</li>
+      //   <li>Get out of the house and be independent</li>
+      //   <li>Being more professional in the workplace</li>
+      // </ol>
+
+        // <h2>Goal log</h2>
+        // <table>
+        //   <tr>
+        //     <th>Date</th>
+        //     <th>Goal worked on</th>
+        //     <th>Log</th>
+        //   </tr>
+        //   <tr>
+        //     <td></td>
+        //     <td></td>
+        //     <td></td>
+        //   </tr>
+        // </table>';
 
       return $body;
-    }
+  }
 }
+$config = new Config();
+$link = $config->connectToServer();
 
 $goals = new Goals();
+$goals->setLink($link);
+$goals->setIsAdmin();
+
 $goals->setUrl($_SERVER['REQUEST_URI']);
 $goals->setTitle('Ephraim Becker - Everyday Life - Goals');
-$goals->setLocalStyleSheet(NULL);
+$goals->setLocalStyleSheet('css/style.css');
 $goals->setLocalScript(NULL);
 $goals->setHeader('Goals');
 $goals->setBody($goals->main());
