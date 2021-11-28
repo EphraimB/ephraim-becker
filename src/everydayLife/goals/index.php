4f0187f6e5b7e4716cf33be4d2b6f99bbe88f52b
Ephraim Becker <emb16@outlook.com>
2021-11-28 14:22:49 -0500

    Deleted unneeded code. #70

--- src/everydayLife/goals/index.php
+++ src/everydayLife/goals/index.php
@@ -139,15 +139,6 @@ class Goals extends Base
     $body .= $this->addGoalLog();
     $body .= $this->goalLog();
 
-    // $body = '<ol style="text-align: left;">
-      //   <li>Get out of defense mode (hopefully with some course in <a target="_blank" rel="noopener" href="https://www.aspergerexperts.com/">Aspergers Experts</a>)</li>
-      //   <li>Become better at socializing</li>
-      //   <li>Expressing myself more</li>
-      //   <li>Mindfulness</li>
-      //   <li>Get out of the house and be independent</li>
-      //   <li>Being more professional in the workplace</li>
-      // </ol>
-
         // <h2>Goal log</h2>
         // <table>
         //   <tr>
