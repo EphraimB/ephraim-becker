88efe5440c6a132d326dbb3dfe8779c9bffc0e3e
Ephraim Becker <emb16@outlook.com>
2022-01-25 14:06:49 -0500

    Switched the attribute to icons. #82

--- src/base.php
+++ src/base.php
@@ -125,16 +125,16 @@ class Base
          data-wheelnav-navangle="270"
          data-wheelnav-titleheight="45"
          data-wheelnav-init>
-          <div data-wheelnav-navitemimg="/img/nav-icons/home_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/index.php">Home</a></div>
-          <div data-wheelnav-navitemimg="/img/nav-icons/timeline_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/timeline/">Timeline</a></div>
-          <div data-wheelnav-navitemimg="/img/nav-icons/life/sentiment_dissatisfied_black_24dp.svg"><a href="javascript:;"></a></div>
-          <div data-wheelnav-navitemimg="/img/nav-icons/code_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/projects/">Projects</a></div>
-          <div data-wheelnav-navitemimg="/img/nav-icons/link_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/resources/">Resources</a></div>
-          <div data-wheelnav-navitemimg="/img/nav-icons/info_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/about/">About</a></div>';
+          <div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/home_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/index.php">Home</a></div>
+          <div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/timeline_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/timeline/">Timeline</a></div>
+          <div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/life/sentiment_dissatisfied_black_24dp.svg"><a href="javascript:;"></a></div>
+          <div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/code_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/projects/">Projects</a></div>
+          <div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/link_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/resources/">Resources</a></div>
+          <div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/info_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/about/">About</a></div>';
           if(isset($_SESSION['username'])) {
-            $html .= '<div data-wheelnav-navitemimg="/img/nav-icons/logout_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/adminLogout.php?fromUrl=' . $this->getUrl() . '">Logout</a></div>';
+            $html .= '<div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/logout_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/adminLogout.php?fromUrl=' . $this->getUrl() . '">Logout</a></div>';
           } else {
-            $html .= '<div data-wheelnav-navitemimg="/img/nav-icons/login_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/adminLogin/index.php?fromUrl=' . $this->getUrl() . '">Login</a></div>';
+            $html .= '<div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/login_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/adminLogin/index.php?fromUrl=' . $this->getUrl() . '">Login</a></div>';
           }
 
       $html .= '</div>
@@ -144,8 +144,8 @@ class Base
         data-wheelnav-navangle="270"
         data-wheelnav-titleheight="50"
         data-wheelnav-init>
-        <div data-wheelnav-navitemimg="/img/nav-icons/life/sentiment_dissatisfied_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/everydayLife/">Everyday Life</a></div>
-        <div data-wheelnav-navitemimg="/img/nav-icons/school/school_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/college/">College Life</a></div>
+        <div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/sentiment_dissatisfied_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/everydayLife/">Everyday Life</a></div>
+        <div data-wheelnav-navitemicon="imgsrc:/img/nav-icons/school_black_24dp.svg"><a href="' . $this->getDocumentRoot() . '/college/">College Life</a></div>
       </div>
     </nav>';
 
