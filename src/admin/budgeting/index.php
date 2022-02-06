8d2dbafa3847f42881911a72ea71d2b3fe2a5b5b
Ephraim Becker <emb16@outlook.com>
2022-02-06 18:37:34 -0500

    The budgeting page now says the current balance. #87

---
+++ src/admin/budgeting/index.php
@@ -0,0 +1,86 @@
+<?php
+declare(strict_types=1);
+
+session_start();
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/environment.php');
+require($_SERVER['DOCUMENT_ROOT'] . "/base.php");
+
+class Budgeting extends Base
+{
+  private $isAdmin;
+  private $link;
+
+  function __construct()
+  {
+    $this->setIsAdmin();
+
+    if(!$this->getIsAdmin()) {
+      header("location: ../../");
+    }
+  }
+
+  function setIsAdmin(): void
+  {
+    if(isset($_SESSION['username'])) {
+      $this->isAdmin = true;
+    } else {
+      $this->isAdmin = false;
+    }
+  }
+
+  function getIsAdmin(): bool
+  {
+    return $this->isAdmin;
+  }
+
+  function setLink($link)
+  {
+    $this->link = $link;
+  }
+
+  function getLink()
+  {
+    return $this->link;
+  }
+
+  function currentBalance(): string
+  {
+    $sql = "SELECT (SELECT SUM(DepositAmount) from deposits) - (SELECT SUM(WithdrawalAmount) FROM withdrawals) AS currentBalance";
+    $sqlResult = mysqli_query($this->getLink(), $sql);
+
+    if(mysqli_num_rows($sqlResult) > 0) {
+      while($row = mysqli_fetch_array($sqlResult)){
+        $currentBalance = $row['currentBalance'];
+      }
+    }
+
+    if(is_null($currentBalance)) {
+      $currentBalance = 0.00;
+    }
+
+    $html = '<h2>Current balance: $' . $currentBalance . '</h2>';
+
+    return $html;
+  }
+
+  function main(): string
+  {
+    $html = $this->currentBalance();
+
+    return $html;
+  }
+}
+$config = new Config();
+$link = $config->connectToServer();
+
+$budgeting = new Budgeting();
+$budgeting->setLink($link);
+$budgeting->setTitle("Ephraim Becker - Admin - Budgeting");
+$budgeting->setLocalStyleSheet('css/style.css');
+$budgeting->setLocalScript(NULL);
+$budgeting->setHeader('Admin - Budgeting');
+$budgeting->setUrl($_SERVER['REQUEST_URI']);
+$budgeting->setBody($budgeting->main());
+
+$budgeting->html();
