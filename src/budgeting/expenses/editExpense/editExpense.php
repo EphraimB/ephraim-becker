cb18ba4081b028f42660b0622b9dc3047640420a
Ephraim Becker <emb16@outlook.com>
2022-05-30 10:16:53 -0400

    Editing expenses now edits the cron job. #110

--- src/budgeting/expenses/editExpense/editExpense.php
+++ src/budgeting/expenses/editExpense/editExpense.php
@@ -9,6 +9,7 @@ class EditExpense
 {
   private $isAdmin;
   private $link;
+  private $cronTabManager;
   private $id;
   private $price;
   private $expenseTitle;
@@ -49,6 +50,16 @@ class EditExpense
   function getLink()
   {
     return $this->link;
+  }
+
+  function setCronTabManager($cronTabManager)
+  {
+    $this->cronTabManager = $cronTabManager;
+  }
+
+  function getCronTabManager()
+  {
+    return $this->cronTabManager;
   }
 
   function setId($id): void
@@ -131,6 +142,53 @@ class EditExpense
     return $this->frequency;
   }
 
+  function getCronJobId(): int
+  {
+    $sql = $this->getLink()->prepare("SELECT CronJobId FROM expenses WHERE ExpenseId=?");
+    $sql->bind_param('i', $id);
+
+    $id = $this->getId();
+
+    $sql->execute();
+
+    $sqlResult = $sql->get_result();
+
+    while($row = mysqli_fetch_array($sqlResult)) {
+      $id = $row['CronJobId'];
+    }
+
+    return $id;
+  }
+
+  function getCronJobUniqueId(): string
+  {
+    $sql = $this->getLink()->prepare("SELECT UniqueId FROM CronJobs WHERE CronJobId=?");
+    $sql->bind_param('i', $id);
+
+    $id = $this->getCronJobId();
+
+    $sql->execute();
+
+    $sqlResult = $sql->get_result();
+
+    while($row = mysqli_fetch_array($sqlResult)) {
+      $uniqueId = $row['UniqueId'];
+    }
+
+    return $uniqueId;
+  }
+
+  function updateCronJobInDB($command): void
+  {
+     $sql = $this->getLink()->prepare("UPDATE CronJobs SET Command=?, DateModified=? WHERE CronJobId=?");
+     $sql->bind_param('ssi', $command, $dateNow, $cronJobId);
+
+     $dateNow = date("Y-m-d H:i:s");
+     $cronJobId = $this->getCronJobId();
+
+     $sql->execute();
+  }
+
   function editExpense(): void
   {
     $sql = $this->getLink()->prepare("UPDATE expenses SET DateModified=?, ExpenseTitle=?, ExpensePrice=?, ExpenseBeginDate=?, timezone=?, timezoneOffset=?, ExpenseEndDate=?, FrequencyOfExpense=? WHERE ExpenseId=?");
@@ -156,7 +214,15 @@ class EditExpense
 
     $frequency = $this->getFrequency();
 
+    $uniqueId = $this->getCronJobUniqueId();
+    $command = intval(date("i", strtotime($beginDate))) . ' ' . intval(date("H", strtotime($beginDate))) . ' ' . date("j", strtotime($beginDate)) . ' * * /usr/local/bin/php /home/s8gphl6pjes9/public_html/budgeting/cron/withdrawalCronJob.php withdrawalAmount=' . $price . ' withdrawalDescription=Expenses id=' . $uniqueId;
+    $this->updateCronJobInDB($command);
+
     $sql->execute();
+
+    $crontab = $this->getCronTabManager();
+    $crontab->remove_cronjob('/' . $uniqueId . '/');
+    $crontab->append_cronjob($command);
 
     $sql->close();
 
@@ -165,9 +231,11 @@ class EditExpense
 }
 $config = new Config();
 $link = $config->connectToServer();
+$cronTabManager = $config->connectToCron();
 
 $editExpense = new EditExpense();
 $editExpense->setLink($link);
+$editExpense->setCronTabManager($cronTabManager);
 $editExpense->setId(intval($_POST['id']));
 $editExpense->setPrice(floatval($_POST['price']));
 $editExpense->setExpenseTitle($_POST['expenseTitle']);
