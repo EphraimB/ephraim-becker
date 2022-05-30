30cb99cf9ee92c22e77a025a12964f46227a1ff9
Ephraim Becker <emb16@outlook.com>
2022-05-30 10:06:29 -0400

    Editing food now edits the cron job. #110

--- src/budgeting/expenses/mealPlan/editFood/editFood.php
+++ src/budgeting/expenses/mealPlan/editFood/editFood.php
@@ -109,6 +109,69 @@ class EditFood
     return $this->mealDayId;
   }
 
+  function getMealHour($mealId): int
+  {
+    $mealHour = 0;
+
+    if($mealId == 0) {
+      $mealHour = 8;
+    } else if($mealId == 1) {
+      $mealHour = 12;
+    }
+    else if($mealId == 2) {
+     $mealHour = 5;
+   }
+
+   return $mealHour;
+  }
+
+  function getCronJobId(): int
+  {
+    $sql = $this->getLink()->prepare("SELECT CronJobId FROM MealPlan WHERE MealPlanId=?");
+    $sql->bind_param('i', $id);
+
+    $id = $this->getId();
+
+    $sql->execute();
+
+    $sqlResult = $sql->get_result();
+
+    while($row = mysqli_fetch_array($sqlResult)) {
+      $id = $row['CronJobId'];
+    }
+
+    return $id;
+  }
+
+  function getCronJobUniqueId(): string
+  {
+    $sql = $this->getLink()->prepare("SELECT UniqueId FROM CronJobs WHERE CronJobId=?");
+    $sql->bind_param('i', $id);
+
+    $id = $this->getCronJobId();
+
+    $sql->execute();
+
+    $sqlResult = $sql->get_result();
+
+    while($row = mysqli_fetch_array($sqlResult)) {
+      $uniqueId = $row['UniqueId'];
+    }
+
+    return $uniqueId;
+  }
+
+  function updateCronJobInDB($command): void
+  {
+     $sql = $this->getLink()->prepare("UPDATE CronJobs SET Command=?, DateModified=? WHERE CronJobId=?");
+     $sql->bind_param('ssi', $command, $dateNow, $cronJobId);
+
+     $dateNow = date("Y-m-d H:i:s");
+     $cronJobId = $this->getCronJobId();
+
+     $sql->execute();
+  }
+
   function editFood(): string
   {
      $sql = $this->getLink()->prepare("UPDATE MealPlan SET MealId=?, MealItem=?, MealPrice=?, MealDayId=?, DateModified=? WHERE MealPlanId=?");
@@ -121,7 +184,15 @@ class EditFood
      $mealDayId = $this->getMealDayId();
      $id = $this->getId();
 
+     $uniqueId = $this->getCronJobUniqueId();
+     $command = '0 ' . $this->getMealHour($mealId) . ' * * ' . $this->getMealDayId() . ' /usr/local/bin/php /home/s8gphl6pjes9/public_html/budgeting/cron/withdrawalCronJob.php withdrawalAmount=' . $mealPrice . ' withdrawalDescription=Meal\ Expenses id=' . $uniqueId;
+     $this->updateCronJobInDB($command);
+
      $sql->execute();
+
+     $crontab = $this->getCronTabManager();
+     $crontab->remove_cronjob('/' . $uniqueId . '/');
+     $crontab->append_cronjob($command);
 
      $sql->close();
      $this->getLink()->close();
