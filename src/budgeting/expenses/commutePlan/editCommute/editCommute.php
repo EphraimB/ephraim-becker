bea69cfc9c4d3e095391453b095089590589ca4a
Ephraim Becker <emb16@outlook.com>
2022-05-30 09:58:28 -0400

    Editing a commute now edits the cron job. #110

--- src/budgeting/expenses/commutePlan/editCommute/editCommute.php
+++ src/budgeting/expenses/commutePlan/editCommute/editCommute.php
@@ -9,6 +9,7 @@ class EditCommute
 {
   private $isAdmin;
   private $link;
+  private $cronTabManager;
   private $commuteDayId;
   private $commutePeriodId;
   private $peakId;
@@ -47,6 +48,16 @@ class EditCommute
   function getLink()
   {
     return $this->link;
+  }
+
+  function setCronTabManager($cronTabManager)
+  {
+    $this->cronTabManager = $cronTabManager;
+  }
+
+  function getCronTabManager()
+  {
+    return $this->cronTabManager;
   }
 
   function setId($id): void
@@ -109,6 +120,68 @@ class EditCommute
     return $this->price;
   }
 
+  function getCommuteHour($commutePeriodId): int
+  {
+    $commuteHour = 0;
+
+    if($commutePeriodId == 0) {
+      $commuteHour = 8;
+    } else if($commutePeriodId == 1) {
+      $commuteHour = 12;
+    } else if($commutePeriodId == 2) {
+      $commuteHour = 5;
+   }
+
+   return $commuteHour;
+  }
+
+  function getCronJobId(): int
+  {
+    $sql = $this->getLink()->prepare("SELECT CronJobId FROM CommutePlan WHERE CommutePlanId=?");
+    $sql->bind_param('i', $id);
+
+    $id = $this->getId();
+
+    $sql->execute();
+
+    $sqlResult = $sql->get_result();
+
+    while($row = mysqli_fetch_array($sqlResult)) {
+      $id = $row['CronJobId'];
+    }
+
+    return $id;
+  }
+
+  function getCronJobUniqueId(): string
+  {
+    $sql = $this->getLink()->prepare("SELECT UniqueId FROM CronJobs WHERE CronJobId=?");
+    $sql->bind_param('i', $id);
+
+    $id = $this->getCronJobId();
+
+    $sql->execute();
+
+    $sqlResult = $sql->get_result();
+
+    while($row = mysqli_fetch_array($sqlResult)) {
+      $uniqueId = $row['UniqueId'];
+    }
+
+    return $uniqueId;
+  }
+
+  function updateCronJobInDB($command): void
+  {
+     $sql = $this->getLink()->prepare("UPDATE CronJobs SET Command=?, DateModified=? WHERE CronJobId=?");
+     $sql->bind_param('ssi', $command, $dateNow, $cronJobId);
+
+     $dateNow = date("Y-m-d H:i:s");
+     $cronJobId = $this->getCronJobId();
+
+     $sql->execute();
+  }
+
   function editCommute(): string
   {
      $sql = $this->getLink()->prepare("UPDATE CommutePlan SET CommuteDayId=?, CommutePeriodId=?, PeakId=?, ZoneOfTransportation=?, Price=?, DateModified=? WHERE CommutePlanId=?");
@@ -123,7 +196,15 @@ class EditCommute
 
      $id = $this->getId();
 
+     $uniqueId = $this->getCronJobUniqueId();
+     $command = '0 ' . $this->getCommuteHour($commutePeriodId) . ' * * ' . $commuteDayId . '  /usr/local/bin/php /home/s8gphl6pjes9/public_html/budgeting/cron/withdrawalCronJob.php withdrawalAmount=' . $price . ' withdrawalDescription=Commute\ Expenses id=' . $uniqueId;
+     $this->updateCronJobInDB($command);
+
      $sql->execute();
+
+     $crontab = $this->getCronTabManager();
+     $crontab->remove_cronjob('/' . $uniqueId . '/');
+     $crontab->append_cronjob($command);
 
      $sql->close();
      $this->getLink()->close();
@@ -133,9 +214,11 @@ class EditCommute
 }
 $config = new Config();
 $link = $config->connectToServer();
+$cronTabManager = $config->connectToCron();
 
 $editCommute = new EditCommute();
 $editCommute->setLink($link);
+$editCommute->setCronTabManager($cronTabManager);
 $editCommute->setCommuteDayId(intval($_POST['commuteDayId']));
 $editCommute->setCommutePeriodId(intval($_POST['commutePeriodId']));
 
