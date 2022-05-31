1e5d5bf389faf91739a2370cc08a620b2892291f
Ephraim Becker <emb16@outlook.com>
2022-05-30 20:10:01 -0400

    Trying to get sheets api connected. #92

--- src/budgeting/cron/generateSpreadsheet.php
+++ src/budgeting/cron/generateSpreadsheet.php
@@ -46,18 +46,114 @@ class GenerateSpreadsheet
     return $this->googleClientApiKey;
   }
 
-  function testSpreadsheet(): string
+  /**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+  function getClient()
   {
-    $client = new Google\Client();
-    $client->setApplicationName("Ephraim Becker");
-    $client->setDeveloperKey($this->getGoogleClientApiKey());
-    $client->setAuthConfig('credentials.json');
+      $home = getenv('HOME');
+      $client = new Google_Client();
+      $client->setApplicationName('Ephraim Becker');
+      $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+      $client->setAuthConfig($home . '/credentials.json');
+      $client->setAccessType('offline');
+      $client->setPrompt('select_account consent');
+
+      // Load previously authorized token from a file, if it exists.
+      // The file token.json stores the user's access and refresh tokens, and is
+      // created automatically when the authorization flow completes for the first
+      // time.
+      $tokenPath = 'token.json';
+      if (file_exists($tokenPath)) {
+          $accessToken = json_decode(file_get_contents($tokenPath), true);
+          $client->setAccessToken($accessToken);
+      }
+
+      // If there is no previous token or it's expired.
+      if ($client->isAccessTokenExpired()) {
+          // Refresh the token if possible, else fetch a new one.
+          if ($client->getRefreshToken()) {
+              $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+          } else {
+              // Request authorization from the user.
+              $authUrl = $client->createAuthUrl();
+              printf("Open the following link in your browser:\n%s\n", $authUrl);
+              print 'Enter verification code: ';
+              $authCode = trim(fgets(STDIN));
+
+              // Exchange authorization code for an access token.
+              $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+              $client->setAccessToken($accessToken);
+
+              // Check to see if there was an error.
+              if (array_key_exists('error', $accessToken)) {
+                  throw new Exception(join(', ', $accessToken));
+              }
+          }
+          // Save the token to a file.
+          if (!file_exists(dirname($tokenPath))) {
+              mkdir(dirname($tokenPath), 0700, true);
+          }
+          file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+      }
+      return $client;
+  }
+
+  function connection()
+  {
+    $client = $this->getClient();
+    $service = new Google_Service_Sheets($client);
+
+    // Prints the names and majors of students in a sample spreadsheet:
+    // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+    $spreadsheetId = '1aQUD3MkEMHnwN069EZW9dwsW6OVicOQ89P40nKVQwhI';
+    $range = 'Class Data!A2:E';
+    $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+    $values = $response->getValues();
+
+    if (empty($values)) {
+        print "No data found.\n";
+    } else {
+        print "Name, Major:\n";
+        foreach ($values as $row) {
+            // Print columns A and E, which correspond to indices 0 and 4.
+            printf("%s, %s\n", $row[0], $row[4]);
+        }
+    }
+  }
+
+  function testSpreadsheet(): void
+  {
+    $client = $this->getClient();
+
+    // Your redirect URI can be any registered URI, but in this example
+    // we redirect back to this same page
+    $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+    $client->setRedirectUri($redirect_uri);
+
+    if(isset($_GET['code'])) {
+      $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    }
 
     $service = new Google_Service_Sheets($client);
-    $result = $service->spreadsheets_values->get("1aQUD3MkEMHnwN069EZW9dwsW6OVicOQ89P40nKVQwhI", "A1");
-    $numRows = $result->getValues() != null ? count($result->getValues()) : 0;
+    $values = [
+      [
+          // Cell values ...
+          "Test sucessfull"
+      ],
+    // Additional rows ...
+    ];
 
-    return $numRows;
+    $body = new Google_Service_Sheets_ValueRange([
+      'values' => $values
+    ]);
+
+    $params = [
+        'valueInputOption' => $valueInputOption
+    ];
+
+    $result = $service->spreadsheets_values->update("1aQUD3MkEMHnwN069EZW9dwsW6OVicOQ89P40nKVQwhI", "A1", $body, $params);
   }
 
   function generateSpreadsheet()
@@ -72,4 +168,4 @@ $googleClientApiKey = $config->getGoogleClientApiKey();
 $generateSpreadsheet = new GenerateSpreadsheet();
 $generateSpreadsheet->setLink($link);
 $generateSpreadsheet->setGoogleClientApiKey($googleClientApiKey);
-$generateSpreadsheet->testSpreadsheet();
+$generateSpreadsheet->connection();
