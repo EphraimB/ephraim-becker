972a96ef9b9ec98176659e3b18581025792f5a83
Ephraim Becker <emb16@outlook.com>
2022-06-16 14:38:44 -0400

    Added a php page for deducting money owed. #114

--- src/budgeting/moneyOwed/addMoneyOwed/addMoneyOwed.php
+++ src/budgeting/moneyOwed/addMoneyOwed/addMoneyOwed.php
@@ -195,8 +195,13 @@ class AddMoneyOwed
      $crontab = $this->getCronTabManager();
      $crontab->append_cronjob($command);
 
+     $primaryId = $this->getLink()->insert_id;
+
      $sql->close();
      $this->getLink()->close();
+
+     $commandTwo = intval(date("i", strtotime($date))) . ' ' . intval(date("H", strtotime($date))) . ' ' . date("j", strtotime($date)) . ' * * /usr/local/bin/php /home/s8gphl6pjes9/public_html/budgeting/cron/deductMoneyOwed.php deductAmount=' . $planAmount . ' primaryId=' . $primaryId . ' id=' . $uniqueId;
+     $crontab->append_cronjob($commandTwo);
 
      header("location: ../");
   }
